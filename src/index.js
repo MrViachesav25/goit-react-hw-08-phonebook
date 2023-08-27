ce60f6d6a280d8ac50a8356744a17b4db96ef82e
Viacheslav Dziubenko <97623457+MrViachesav25@users.noreply.github.com>
2023-08-27 23:28:07 +0300

    add com

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import '@fontsource-variable/rubik';
 import App from 'App';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
